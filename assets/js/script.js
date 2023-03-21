5997f90b3457d2934627763b4d77101451bb60ad
GeorgeHazaka <hazaka.george@gmail.com>
2023-03-21 17:43:01 +0000

    Declare the variables needed to code who is the winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,41 @@ const playerDiv = document.getElementById("player-weapon-div");
 const computerDiv = document.getElementById("computer-weapon-div");
 const drawDiv = document.getElementById("draw-message");
 const drawPara = document.createElement("p");
+const playerWeapon = document.getElementById("player-weapon-image");
+const computerWeapon = document.getElementById("computer-weapon-image");
+
 let computerRandomWeapon;
+
+const rockWin = document.createElement("img");
+const rockLose = document.createElement("img");
+const paperWin = document.createElement("img");
+const paperLose = document.createElement("img");
+const scissorsWinRight = document.createElement("img");
+const scissorsWinLeft = document.createElement("img");
+const scissorsLoseRight = document.createElement("img");
+const scissorsLoseLeft = document.createElement("img");
+
+const weaponsArray = [rockWin, rockLose, paperWin, paperLose, scissorsWinRight, scissorsWinLeft, scissorsLoseRight, scissorsLoseLeft];
+const imagesSourcesArray = [
+    "assets/images/Win-Rock.png",
+    "assets/images/Lose-Rock.png",
+    "assets/images/Win-Paper.png", "assets/images/Lose-Paper.png",
+    "assets/images/Win-Scissors-Right.png",
+    "assets/images/Win-Scissors-Left.png",
+    "assets/images/Lose-Scissors-Right.png",
+    "assets/images/Lose-Scissors-Left.png"
+];
+const removePlayer = [];
+const removeComputer = [];
+const winningTeam = [];
+const losingTeam = [];
+
+for (let i = 0; i < weaponsArray.length; i++) {
+    weaponsArray[i].setAttribute("src", `${imagesSourcesArray[i]}`);
+}
+
+winningTeam.push(rockWin, paperWin,scissorsWinRight, scissorsWinLeft);
+losingTeam.push(rockLose, paperLose, scissorsLoseRight, scissorsLoseLeft);
 
 for (let button of buttons) {
     button.addEventListener("click", function () {
@@ -31,6 +65,8 @@ for (let button of buttons) {
             drawDiv.appendChild(drawPara);
             playerDiv.style.display = "none";
             computerDiv.style.display = "none";
+        } else if (parseInt(this.getAttribute("data-choice")) - computerRandomWeapon === 1 || parseInt(this.getAttribute("data-choice")) - computerRandomWeapon === -1) {
+
         }
     })
 }
